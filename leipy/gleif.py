44f98385cf6ff2f03f3937e848db9f3b51571494
Emre Can <emredjan@gmail.com>
2017-12-04 22:45:00 +0100

    initial api logic

--- leipy/gleif.py
+++ leipy/gleif.py
@@ -2,21 +2,25 @@ import requests
 
 class GLEIF:
 
-    gleif_api_version = 'v1'
-    gleif_api_url = f'https://leilookup.gleif.org/api/{gleif_api_version}/leirecords'
-    gleif_api_param = 'lei'
+    api_version = 'v1'
+    api_url = f'https://leilookup.gleif.org/api/{api_version}/leirecords'
+    api_param = 'lei'
 
     def __init__(self, lei_list):
         self.lei_list = lei_list
 
     def process_leis(self):
         chunk_size = 50
+        output = []
+        
         for i in range(0, len(self.lei_list), chunk_size):
             # the API needs comma separated list of LEIs
             leis_to_process = ','.join(self.lei_list[i:i+chunk_size])
 
             # make the actual request
-            payload = {GLEIF.gleif_api_param: leis_to_process}
-            request = requests.get(GLEIF.gleif_api_url, params=payload)
+            payload = {GLEIF.api_param: leis_to_process}
+            request = requests.get(GLEIF.api_url, params=payload)
 
-            return request.json()
+            output += request.json()
+        
+        return output
--- tests/test_leipy.py
+++ tests/test_leipy.py
@@ -1,8 +1,17 @@
 from leipy import GLEIF
 
+import csv
+with open('example_lei.csv', 'r') as f:
+    reader = csv.reader(f)
+    l_list = [i[0] for i in reader]
+
 def test_lei_name():
-    g = GLEIF(['261700K5E45DJCF5Z735'])
+    g = GLEIF(l_list)
 
     output = g.process_leis()
 
-    assert output[0]['Entity']['LegalName']['$'] == 'APIR SYSTEMS LIMITED'
+    assert isinstance(output, list)
+    assert 'KS Projekt GmbH' in [output[i]['Entity']['LegalName']['$'] for i in range(len(output))]
+    assert 'Rimatzki Baugeschäft GmbH' in [output[i]['Entity']['LegalName']['$'] for i in range(len(output))]
+    assert len(output) == len(g.lei_list)
+    assert len(output[0]) >= 3
